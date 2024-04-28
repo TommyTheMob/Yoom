ed4cce285147b852970f6918b56df6a227bb9fe2
TommyTheMob <zv3rbrus@gmail.com>
2024-04-28 18:46:04 +0300

    create call page; implement schedule meetings

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import StreamVideoProvider from "@/providers/StreamClientProvider";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling app",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
